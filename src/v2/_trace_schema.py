52846b4ac8282ba37736f833f0193aa853331b86
cc <dev@guangfusi.com>
2026-05-02 23:19:40 +0800

    feat(v2): agent_b polish adds 2-3 issue mandate + kept_as_is opt-out

--- src/v2/_trace_schema.py
+++ src/v2/_trace_schema.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 _KEYS = (
-    "portrait", "brief", "lyrics", "style", "exclude",
-    "selected_ids", "anchor_source_paths", "anchor_used", "review_skipped",
+    "portrait", "lyrics", "style", "exclude", "emotion_focus", "persona_used",
+    "selected_ids", "anchor_source_paths", "anchor_used", "anchor_song_name", "anchor_chorus_status",
+    "review_skipped", "review_decision", "review_reason", "lyrics_changed", "polish_passes", "polish_diffs",
+    "platform_adapt_status", "platform_adapt_raw_response",
     "llm_total_input_tokens", "llm_total_output_tokens", "llm_total_calls",
 )
 _LIST_KEYS = frozenset(())
